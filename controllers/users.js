04c1e4a5bd7a86986ee628626feed7059b598aed
Natalya Radina <igronat@yandex.ru>
2022-04-14 11:26:51 +0300

    исправлены наименование ошибок-2

--- controllers/users.js
+++ controllers/users.js
@@ -7,39 +7,77 @@ module.exports.createUser = (req, res) => {
     // вернём записанные в базу данные
     .then(user => res.send({ data: user }))
     // данные не записались, вернём ошибку
-    .catch(() => res.status(400).send({ message: 'Переданы некорректные данные' }));
+    .catch(() => {
+      if (!User[req.body]) {
+        res.status(400).send({ message: 'Переданы некорректные данные' })
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    })
+
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then(user => res.send({ data: user }))
-    .catch(() => res.status(404).send({ message: 'Пользователи не найдены' }));
+    .catch(() => {
+      if (!User) {
+        res.status(400).send({ message: 'Переданы некорректные данные' })
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' })});
 };
 
 module.exports.getUserId = (req, res) => {
   User.findById(req.params.id)
     .then(user => res.send({data: user}))
-    .catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
+    .catch(() => {
+      if (!User[req.params.id]) {
+        res.status(404).send({ message: 'Пользователь не найден' })
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' })});
 };
 
 module.exports.updateUser = (req, res) => {
   const { name, about} = req.body;
+
   User.findByIdAndUpdate(req.params.id, {name, about},
     {
       new: true, // обработчик then получит на вход обновлённую запись
       runValidators: true, // данные будут валидированы перед изменением
     })
     .then(user => res.send({data: user}))
-    .catch(() => res.status(400).send({ message: 'Переданы некорректные данные' }));
+    .catch(() => {
+      if (!User[req.params.id]) {
+        res.status(404).send({ message: 'Пользователь не найден' })
+        return;
+
+      }
+      if (!User[req.body]) {
+        res.status(400).send({ message: 'Переданы некорректные данные' })
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' })});
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar} = req.body;
-  User.findByIdAndUpdate(req.user.id, {avatar},
+  User.findByIdAndUpdate(req.params.id, {avatar},
     {
       new: true, // обработчик then получит на вход обновлённую запись
       runValidators: true, // данные будут валидированы перед изменением
     })
     .then(user => res.send({data: user}))
-    .catch(() => res.status(400).send({ message: 'Переданы некорректные данные' }));
+    .catch(() => {
+      if (!User[req.params.id]) {
+        res.status(404).send({ message: 'Пользователь не найден' })
+        return;
+      }
+
+      if (!User[req.body]) {
+        res.status(400).send({ message: 'Переданы некорректные данные' })
+        return;
+      }
+      res.status(400).send({ message: 'Переданы некорректные данные' })});
 };
