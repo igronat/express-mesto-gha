f3d709d8cb84296c788eef9415f0654f246c317d
Natalya Radina <igronat@yandex.ru>
2022-04-25 21:13:49 +0300

    исправлены errors в app

--- app.js
+++ app.js
@@ -63,16 +63,18 @@ app.use(errors());
 
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
-  const { statusCode = 500, message } = err;
 
-  res
-    .status(statusCode)
-    .send({
+  const { statusCode = 500, message } = err;
+  next(
+    res
+      .status(statusCode)
+      .send({
       // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
+        message: statusCode === 500
+          ? 'На сервере произошла ошибка'
+          : message,
+      }),
+  );
 });
 
 app.listen(PORT, () => {
